30fe8b7f0a1a9e77fe608d8017dc335f48a4cabd
Daniel Benamor <dbenamor@users.noreply.github.com>
2023-01-20 09:03:24 -0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,125 @@
+﻿
+namespace TelaAtiva
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.BtnStop = new System.Windows.Forms.Button();
+            this.BtnPlay = new System.Windows.Forms.Button();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.toolTip2 = new System.Windows.Forms.ToolTip(this.components);
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.lblContador = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // BtnStop
+            // 
+            this.BtnStop.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.BtnStop.Image = global::TelaAtiva.Properties.Resources.music_stop;
+            this.BtnStop.Location = new System.Drawing.Point(21, 34);
+            this.BtnStop.Name = "BtnStop";
+            this.BtnStop.Size = new System.Drawing.Size(50, 50);
+            this.BtnStop.TabIndex = 1;
+            this.BtnStop.UseVisualStyleBackColor = true;
+            this.BtnStop.Click += new System.EventHandler(this.BtnStop_Click);
+            // 
+            // BtnPlay
+            // 
+            this.BtnPlay.Image = global::TelaAtiva.Properties.Resources.music_play;
+            this.BtnPlay.Location = new System.Drawing.Point(77, 34);
+            this.BtnPlay.Name = "BtnPlay";
+            this.BtnPlay.Size = new System.Drawing.Size(50, 50);
+            this.BtnPlay.TabIndex = 0;
+            this.BtnPlay.UseVisualStyleBackColor = true;
+            this.BtnPlay.Click += new System.EventHandler(this.BtnPlay_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // lblContador
+            // 
+            this.lblContador.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lblContador.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblContador.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.lblContador.Location = new System.Drawing.Point(3, 22);
+            this.lblContador.Name = "lblContador";
+            this.lblContador.Size = new System.Drawing.Size(155, 57);
+            this.lblContador.TabIndex = 3;
+            this.lblContador.Text = "00:00:00";
+            this.lblContador.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.lblContador);
+            this.groupBox1.Location = new System.Drawing.Point(149, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(161, 82);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Cronômetro";
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.BtnPlay;
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.CancelButton = this.BtnStop;
+            this.ClientSize = new System.Drawing.Size(322, 111);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.BtnStop);
+            this.Controls.Add(this.BtnPlay);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Tela Ativa";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BtnPlay;
+        private System.Windows.Forms.Button BtnStop;
+        private System.Windows.Forms.ToolTip toolTip1;
+        private System.Windows.Forms.ToolTip toolTip2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label lblContador;
+        private System.Windows.Forms.GroupBox groupBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace TelaAtiva
+{
+    public partial class Form1 : Form
+    {
+        Stopwatch cronometro = new Stopwatch();
+        private bool _start;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            BtnPlay.Focus();
+            toolTip1.SetToolTip(BtnPlay, "Start");
+            toolTip2.SetToolTip(BtnStop, "Stop");
+        }
+
+        private void BtnPlay_Click(object sender, EventArgs e)
+        {
+            Start();
+        }
+
+        private void BtnStop_Click(object sender, EventArgs e)
+        {
+            Stop();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            lblContador.Text = String.Format("{0:00}:{1:00}:{2:00}", cronometro.Elapsed.Hours, cronometro.Elapsed.Minutes, cronometro.Elapsed.Seconds);
+        }
+
+        private void Start()
+        {
+            _start = true;
+            cronometro.Reset();
+            cronometro.Start();
+
+            while (_start == true)
+            {
+                SendKeys.Send("F5");
+
+                Thread.Sleep(1000);
+                Application.DoEvents();
+            }
+        }
+
+        private void Stop()
+        {
+            _start = false;
+            cronometro.Stop();
+            cronometro.Reset();
+        }
+
+    }
+}
